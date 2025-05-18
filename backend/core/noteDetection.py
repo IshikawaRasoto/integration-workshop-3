bd403038f91568b03e70833a0e92598fdb0bb0c3
brunopc <bezenatti@hotmail.com>
2025-05-18 17:18:03 -0300

    added mock of notes

--- backend/core/noteDetection.py
+++ backend/core/noteDetection.py
@@ -1,3 +1,32 @@
+import asyncio
+import random
+
+#TODO hardcode these notes that I don't know what I am doing.
+NOTES = ["C4", "C#4", "Db4", "D4", "D#4", "Eb4", "E4", "F4", "F#4", "Gb4", "G4", "G#4", "Ab4", "A4", "A#4", "Bb4", "B4", "None"]
+DURATIONS = ["quarter", "half", "whole"]  
+
+MOCK_RESULTS = []
+for i in range(16):
+    note = random.choice(NOTES)
+    duration = random.choice(DURATIONS) if note != "None" else ""
+    MOCK_RESULTS.append((note, duration))
+print(f"[NoteDetection] Mocked Board Setup: {MOCK_RESULTS}")
+
+#Must return the note with its respective duration
+async def detect_note_for_column(column_index: int) -> tuple[str, str]:
+    await asyncio.sleep(0.005)  # simulate delay
+
+    #TODO really detect 
+    note, duration = MOCK_RESULTS[column_index]
+    # print(f"[NoteDetection] Column {column_index}: {note}, {duration}")
+    return (note, duration)
+
+#Must return all notes with its durations
+async def detectBoardNotes() -> list[tuple[str, str]]:
+    results = await asyncio.gather(*(detect_note_for_column(i) for i in range(16)))
+    # print(f"[NoteDetection] Board Detected: {results}")
+    return results
+
 #TODO detection of colors
 #TODO detection of position
 #TODO integrate both
--- backend/modes/identify.py
+++ backend/modes/identify.py
@@ -3,17 +3,20 @@ from core import session
 
 async def run_mode():
     print("[IdentifyMode] Entered Identify Note Mode")
+    try:
+        while session.state.page == "identify":
+            # TODO: Read current note from melody using note_index
+            # TODO: Use noteDetection to read placed note
+            # TODO: Compare detected note to expected note
 
-    while session.state.page == "identify":
-        # TODO: Read current note from melody using note_index
-        # TODO: Use noteDetection to read placed note
-        # TODO: Compare detected note to expected note
+            # TODO: Green LED if correct, advance index
+            # TODO: Red LED if incorrect
+            # TODO: Yellow LED if nothing detected
+            await asyncio.sleep(0.5) # avoid CPU overuse (this mode doesn't need to be so fast)
 
-        # TODO: Green LED if correct, advance index
-        # TODO: Red LED if incorrect
-        # TODO: Yellow LED if nothing detected
 
+    except asyncio.CancelledError: # when ModeManager does .cancel()
+        print("[IdentifyMode] Exited Identify Note Mode")
+        raise
         
-        await asyncio.sleep(0.5) # avoid CPU overuse (this mode doesn't need to be so fast)
 
-    print("[IdentifyMode] Exited Identify Note Mode")
