8e581328da2040022151c3ce878b58a8a1a6c744
brunopc <bezenatti@hotmail.com>
2025-05-18 15:59:02 -0300

    working mode redirecting

--- backend/modes/idle.py
+++ backend/modes/idle.py
@@ -3,11 +3,15 @@ from core import session
 
 async def run_mode():
     print("[IdleMode] Entered Idle/Home Mode")
+    try:
+        while session.state.page == "home":
+            if session.state.accessibility:
+                print("Turn on ESP32 Communication")  # TODO: esp32 logic
+            await asyncio.sleep(1)
+        print("[IdleMode] Detected page-change in loop")
 
-    while session.state.page == "home":
-        if session.state.accessibility:
-            print("Turn on ESP32 Communication") #TODO 
-        
-        await asyncio.sleep(1)
-
-    print("[IdleMode] Exited Idle/Home Mode")
+    except asyncio.CancelledError: # when ModeManager does .cancel()
+        print("[IdleMode] Task was cancelled")
+        raise
+    finally:
+        print("[IdleMode] Exited Idle Mode")
