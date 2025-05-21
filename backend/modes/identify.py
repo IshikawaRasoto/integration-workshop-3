92fa890faab5d015096c45c968c7650fcf39acd0
brunopc <bezenatti@hotmail.com>
2025-05-21 14:13:33 -0300

    started hear and identify logic

--- backend/modes/identify.py
+++ backend/modes/identify.py
@@ -5,14 +5,17 @@ async def run_mode():
     print("[IdentifyMode] Entered Identify Note Mode")
     try:
         while session.state.page == "identify":
-            # TODO: Read current note from melody 
-            # TODO: Use noteDetection to read placed note
-            # TODO: Compare detected note to expected note
+            while(session.state.playing and session.state.melody is not None):
+                print("loop")
+                # TODO: Read current note from melody using note_index
+                # TODO: Use noteDetection to read placed note
+                # TODO: Compare detected note to expected note
 
-            # TODO: Green LED if correct, advance index
-            # TODO: Red LED if incorrect
-            # TODO: Yellow LED if nothing detected
-            await asyncio.sleep(0.5) # avoid CPU overuse (this mode doesn't need to be so fast)
+                # TODO: Green LED if correct, advance index
+                # TODO: Red LED if incorrect
+                # TODO: Yellow LED if nothing detected
+                await asyncio.sleep(0.5) # NECESSARY TO DO THE OTHER TASKS (ModeManager etc)
+            await asyncio.sleep(1) # NECESSARY TO DO THE OTHER TASKS (ModeManager etc)
 
 
     except asyncio.CancelledError: # when ModeManager does .cancel()
