e5c444054af9e76fe26c614fcf6beb794929aa82
brunopc <bezenatti@hotmail.com>
2025-05-21 14:13:33 -0300

    started hear and identify logic

--- backend/modes/hear.py
+++ backend/modes/hear.py
@@ -4,16 +4,19 @@ from core import session
 async def run_mode():
     print("[HearMode] Entered Hear Note Mode")
     try:
-        while session.state.page == "hear":
-            # TODO: Play the current note from the melody
-            # TODO: Wait for user to place matching note
-            # TODO: Use noteDetection to check if it matches
+        while (session.state.page == "hear"):
+            while(session.state.playing and session.state.melody is not None):
+                print("loop")
+                # TODO: Play the current note from the melody
+                # TODO: Wait for user to place matching note
+                # TODO: Use noteDetection to check if it matches
 
-            # TODO: Green LED if correct, advance index
-            # TODO: Red LED if incorrect
-            # TODO: Yellow LED if nothing detected
+                # TODO: Green LED if correct, advance index
+                # TODO: Red LED if incorrect
+                # TODO: Yellow LED if nothing detected
+                await asyncio.sleep(0.5)  # NECESSARY TO DO THE OTHER TASKS (ModeManager etc)
 
-            await asyncio.sleep(0.5)  # avoid CPU overuse (this mode doesn't need to be so fast)
+            await asyncio.sleep(1)  # NECESSARY TO DO THE OTHER TASKS (ModeManager etc)
             
     except asyncio.CancelledError: # when ModeManager does .cancel()
         print("[HearMode] Exited Hear Note Mode")
